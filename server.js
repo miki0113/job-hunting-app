b95ab7a587124288430511e3ea3683c40b390066
miki0113 <120442163+miki0113@users.noreply.github.com>
2026-05-23 11:45:09 +0900

    server.js を更新

--- server.js
+++ server.js
@@ -5,12 +5,8 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }
-});
+const pool = new Pool({ connectionString: process.env.DATABASE_URL, ssl: { rejectUnauthorized: false } });
 
-// テーブル構築
 pool.query(`
   CREATE TABLE IF NOT EXISTS job_list (
     id SERIAL PRIMARY KEY,
@@ -21,13 +17,11 @@ pool.query(`
   );
 `).catch(console.error);
 
-// データの取得
 app.get('/api/jobs', async (req, res) => {
   const result = await pool.query('SELECT * FROM job_list ORDER BY id ASC');
   res.json(result.rows);
 });
 
-// データの追加
 app.post('/api/jobs', async (req, res) => {
   const { company_name, closest_station, memo, status } = req.body;
   await pool.query('INSERT INTO job_list (company_name, closest_station, memo, status) VALUES ($1, $2, $3, $4)', 
@@ -35,7 +29,6 @@ app.post('/api/jobs', async (req, res) => {
   res.send('Success');
 });
 
-// 削除
 app.delete('/api/jobs/:id', async (req, res) => {
   await pool.query('DELETE FROM job_list WHERE id = $1', [req.params.id]);
   res.send('Deleted');
